a8c8d0ba9667e3055d0877461dac4079c88a3117
Michael Singh <singh.michaelanthony@gmail.com>
2020-07-08 19:12:23 -0400

    query select form inputs

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 $( document ).ready(() => {
     console.log('ready');
+
+    $( '#submit' ).on('click', () => {
+        console.log($( '#username' ).val());
+        console.log($( '#password' ).val());
+        console.log($( '#email' ).val());
+    });
 });
