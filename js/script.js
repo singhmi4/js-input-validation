9378c2e570b7f8999673a7c1950a0ce46739ead7
Michael Singh <singh.michaelanthony@gmail.com>
2020-07-09 14:02:20 +0000

    implement username regex test

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 $( document ).ready(() => {
     console.log('ready');
+	
+	$( '#username' ).on('blur', function() {
+		const usernameRegex = /^[a-zA-Z]+$/;
+		const usernameResult = usernameRegex.test($(this).val())
+		console.log($(this).val());
+		console.log(usernameResult);
+	});
 
     $( '#submit' ).on('click', () => {
         console.log($( '#username' ).val());
